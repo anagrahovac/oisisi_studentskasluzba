891b6e43b15038cbbbf6afc1c66f4e37e543d4ca
lenche23 <aleksic.ra23.2018@uns.ac.rs>
2020-11-29 01:52:28 +0100

    #menu_bar Dodata klasa MyMenuBar

---
+++ StudentskaSluzba/src/view/MyMenuBar.java
@@ -0,0 +1,58 @@
+package view;
+
+import javax.swing.JMenuBar;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+//import javax.swing.ImageIcon;
+
+
+public class MyMenuBar extends JMenuBar{
+
+	public MyMenuBar() {
+		setVisible(true);
+		
+		JMenu menuFile = new JMenu("File");
+		JMenu menuEdit = new JMenu("Edit"); 
+		JMenu menuHelp = new JMenu("Help"); 
+		
+		menuFile.setMnemonic(KeyEvent.VK_F);
+		menuEdit.setMnemonic(KeyEvent.VK_E);
+		menuHelp.setMnemonic(KeyEvent.VK_H);
+		
+		JMenuItem menuFileNew = new JMenuItem("New", new ImageIcon("menubar_images/new.png"));
+		menuFileNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.ALT_MASK));		
+		JMenuItem menuFileClose = new JMenuItem("Close", new ImageIcon("menubar_images/close.png"));
+		menuFileClose.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.ALT_MASK));
+		
+		JMenuItem menuEditEdit = new JMenuItem("Edit",new ImageIcon("menubar_images/edit.png"));
+		menuEditEdit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.ALT_MASK));
+		JMenuItem menuEditDelete = new JMenuItem("Delete",new ImageIcon("menubar_images/delete.png"));
+		menuEditDelete.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, ActionEvent.ALT_MASK));
+		
+		JMenuItem menuHelpHelp = new JMenuItem("Help",new ImageIcon("menubar_images/help.png"));
+		menuHelpHelp.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, ActionEvent.ALT_MASK));
+		JMenuItem menuHelpAbout = new JMenuItem("About",new ImageIcon("menubar_images/about.png"));
+		menuHelpAbout.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.ALT_MASK));
+		
+		menuFile.add(menuFileNew);
+		menuFile.add(menuFileClose);
+		
+		menuEdit.add(menuEditEdit);
+		menuEdit.add(menuEditDelete);
+		
+		menuHelp.add(menuHelpHelp);
+		menuHelp.add(menuHelpAbout);
+		
+		add(menuFile);
+		add(menuEdit);
+		add(menuHelp);
+
+	}
+}
+
