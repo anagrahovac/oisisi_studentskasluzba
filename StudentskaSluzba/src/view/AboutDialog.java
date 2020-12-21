16bffadc37af366b4a4c41782b00972ad14a93cb
lenche23 <aleksic.ra23.2018@uns.ac.rs>
2020-12-21 18:36:10 +0100

    #menu_bar Dodate klase za otvaranje submenija Help i About

---
+++ StudentskaSluzba/src/view/AboutDialog.java
@@ -0,0 +1,66 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Toolkit;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class AboutDialog extends JDialog{
+	
+	public AboutDialog(JFrame parent, String title) {
+
+	super(parent,"About");
+	setLayout(new BorderLayout());
+	
+	Font f = new Font("Dialog",Font.ITALIC,14);
+	Font f1 = new Font("Dialog",Font.PLAIN,13);
+	Color c = Color.white;	
+	
+	Toolkit kit = Toolkit.getDefaultToolkit();
+	Dimension screenSize = kit.getScreenSize();
+	int sizeHeight = (screenSize.height * 3 / 4) * 3/4;
+	int sizeWidth = (screenSize.width * 3 / 4) / 2;
+	
+	setSize(sizeWidth, sizeHeight);
+	setLocationRelativeTo(parent);
+	setBackground(c);
+			
+	
+	JPanel header = new JPanel(new FlowLayout(FlowLayout.LEADING,5,10));
+	header.setBackground(c);
+	this.add(header,BorderLayout.NORTH);
+	
+	JPanel background = new JPanel(new FlowLayout(FlowLayout.LEADING,5,10));
+	background.setBackground(c);
+	this.add(background, BorderLayout.CENTER);
+	
+	JLabel lblHeader = new JLabel("<html><body>Verzija aplikacije:    2020-09 (4.17.0)");
+	lblHeader.setFont(f);
+	
+	JLabel koriscenje = new JLabel(); 
+	koriscenje.setText("<html><body>"
+			+ "Biografija studenta 1:"
+			+ "<br>Aleksić Lenka Isidora<br>"
+			+ "BLABLABLABLABLABLABLABLA<br>"
+			+ "<br><br>"
+			+ "Biografija studenta 2:<br>"
+			+ "Grahovac Ana<br>"
+			+ "BLABLABLALBABLABLABALBLA");
+	koriscenje.setFont(f1);
+	
+	
+	header.setBackground(c);
+	koriscenje.setBackground(c);
+	
+	header.add(lblHeader);
+	background.add(koriscenje);
+	
+	}
+}
---
+++ StudentskaSluzba/src/view/HelpDialog.java
@@ -0,0 +1,71 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Toolkit;
+
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class HelpDialog extends JDialog{
+	
+	
+	public HelpDialog(Frame parent,String title) {
+		super(parent,"Help");
+		setLayout(new BorderLayout());
+		
+		Font f = new Font("Dialog",Font.ITALIC,14);
+		Font f1 = new Font("Dialog",Font.PLAIN,13);
+		Color c = Color.white;	
+		
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize();
+		int sizeHeight = (screenSize.height * 3 / 4) * 3/4;
+		int sizeWidth = (screenSize.width * 3 / 4) / 2;
+		
+		setSize(sizeWidth, sizeHeight);
+		setLocationRelativeTo(parent);
+		setBackground(c);
+				
+		
+		JPanel header = new JPanel(new FlowLayout(FlowLayout.LEADING,5,10));
+		header.setBackground(c);
+		this.add(header,BorderLayout.NORTH);
+		
+		JPanel background = new JPanel(new FlowLayout(FlowLayout.LEADING,5,10));
+		background.setBackground(c);
+		this.add(background, BorderLayout.CENTER);
+		
+		JLabel lblHeader = new JLabel("Uputstvo za koriscenje aplikacije:");
+		lblHeader.setFont(f);
+		
+		JLabel koriscenje = new JLabel(); 
+		koriscenje.setText("<html><body>Pred vama se nalazi aplikacija koja predstavlja studentsku službu"
+				+ "<br>Fakulteta Tehničkih Nauka u Novom Sadu.<br>"
+				+ "BLABLABLABLABLABLABLABLA<br>"
+				+ "<br>"
+				+ "ALT+F - otvara File stavku menija<br>"
+				+ "ALT+E - otvara Edit stavku menija<br>"
+				+ "ALT+H - otvara Help stavku menija<br>"
+				+ "CTRL+N - prečica za dodavanje novog entiteta u sistem<br>"
+				+ "CTRL+C - prečica za zatvaranje aplikacije<br>"
+				+ "CTRL+T - prečica za izmenu entiteta iz sistema<br>"
+				+ "CTRL+D - prečica za brisanje postojećeg entiteta iz sistema<br>"
+				+ "CTRL+B - prečica za otvaranje submenija za pomoć o radu aplikacije<br>"
+				+ "CTRL+A - prečica za otvaranje submenija za opis autora<br>");
+		koriscenje.setFont(f1);
+		
+		
+		header.setBackground(c);
+		koriscenje.setBackground(c);
+		
+		header.add(lblHeader);
+		background.add(koriscenje);
+	}
+
+}
